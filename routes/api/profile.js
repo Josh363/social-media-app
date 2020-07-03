7dbe1a9eba55172a9fa199716d86618072a7147b
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-07-02 20:05:24 -0500

    feat: add new post route/POST schema created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -153,7 +153,7 @@ router.delete('/', auth, async (req, res) => {
     //Remove profile
     await Profile.findOneAndRemove({ user: req.user.id })
     //Remove user
-    await Profile.findOneAndRemove({ _id: req.user.id })
+    await User.findOneAndRemove({ _id: req.user.id })
     res.json({ msg: 'User deleted' })
   } catch (err) {
     console.error(err.message)
