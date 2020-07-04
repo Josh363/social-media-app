81b5af022bf6d054806d63f917c6348eac0232f0
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-07-04 05:09:19 -0500

    feat: added landing page, login, and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
 import Land from './components/layouts/Land'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import './App.css'
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Land />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Land} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </Fragment>
+    </Router>
   )
 }
 export default App
