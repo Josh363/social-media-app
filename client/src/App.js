e0b4323176ef8517294e966db7deb86b38087a25
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-07-06 05:44:37 -0500

    feat: AddEducation and AddExperience components

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './components/layouts/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
 import ProfileForm from './components/profile-forms/ProfileForm'
+import AddExperience from './components/profile-forms/AddExperience'
+import AddEducation from './components/profile-forms/AddEducation'
 import './App.css'
 //Redux
 import { Provider } from 'react-redux'
@@ -47,6 +49,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={ProfileForm}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </section>
           </Switch>
