c72b1c1e52d01cc74bf414846fb339f9a440fd6b
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-07-08 06:33:15 -0500

    feat: display posts, add post, and view post/:id

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types'
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+  GET_POST,
+} from './types'
 
 //Get Posts
 export const getPosts = () => async (dispatch) => {
@@ -41,6 +48,64 @@ export const deleteLike = (postId) => async (dispatch) => {
     dispatch({
       type: UPDATE_LIKES,
       payload: { postId, likes: res.data },
+    })
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+//Delete Post
+export const deletePost = (postId) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/${postId}`)
+    dispatch({
+      type: DELETE_POST,
+      payload: postId,
+    })
+
+    dispatch(setAlert('Post Removed', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+//Add Post
+export const addPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  try {
+    const res = await axios.post(`/api/posts/`, formData, config)
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    })
+
+    dispatch(setAlert('Post Removed', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+//Get Post
+export const getPost = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${postId}`)
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
     })
   } catch (err) {
     dispatch({
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Spinner from '../layouts/Spinner'
+import PostItem from '../posts/PostItem'
+import { getPost } from '../../actions/post'
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id)
+  }, [getPost, match.params.id])
+
+  const onClick = () => {
+    console.log('TODO: Clear Post after clicking the back button')
+  }
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn' onClick={onClick}>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  )
+}
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+})
+
+export default connect(mapStateToProps, { getPost })(Post)
